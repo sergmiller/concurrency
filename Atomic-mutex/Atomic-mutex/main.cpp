5c2f85d43a4c138c466e79c938dfc6252d70d81d
sergmiller <sergey.miller@phystech.edu>
2016-03-15 17:53:07 +0300

    start futex project

---
+++ Atomic-mutex/Atomic-mutex/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Atomic-mutex
+//
+//  Created by Сергей Миллер on 15.03.16.
+//  Copyright © 2016 Serg Miller. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
