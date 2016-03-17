6ea3594eeb983278d0aa2b1abefba5612d432732
sergmiller <sergey.miller@phystech.edu>
2016-03-17 14:29:40 +0300

    deleted

---
+++ mipt-concurrency/mipt-concurrency/futex.cpp
@@ -0,0 +1,9 @@
+//
+//  futex.cpp
+//  mipt-concurrency
+//
+//  Created by Сергей Миллер on 16.03.16.
+//  Copyright © 2016 Serg Miller. All rights reserved.
+//
+
+#include "futex.hpp"
---
+++ mipt-concurrency/mipt-concurrency/futex.hpp
@@ -0,0 +1,14 @@
+//
+//  futex.hpp
+//  mipt-concurrency
+//
+//  Created by Сергей Миллер on 16.03.16.
+//  Copyright © 2016 Serg Miller. All rights reserved.
+//
+
+#ifndef futex_hpp
+#define futex_hpp
+
+#include <stdio.h>
+
+#endif /* futex_hpp */
