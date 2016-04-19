137e2b5e08f4d09b96dfbe5d30ae092988c9d78a
sergmiller <sergey.miller@phystech.edu>
2016-04-19 15:05:32 +0300

    init cache-ping-pong

---
+++ cache-ping-pong/cache-ping-pong/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  cache-ping-pong
+//
+//  Created by Сергей Миллер on 19.04.16.
+//  Copyright © 2016 sergmiller. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
