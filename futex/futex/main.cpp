21df6d227ae831cbd3c6b97a777b6a1345ebf365
sergmiller <sergey.miller@phystech.edu>
2016-04-28 21:12:58 +0300

    testing

--- futex/futex/main.cpp
+++ futex/futex/main.cpp
@@ -15,17 +15,17 @@
 #include "futex.hpp"
 #include "futex_with_memory_barriers.hpp"
 
-#define MAX_THREADS 10
-#define LIMIT (int)3e8
+#define THREADS 4
+#define LIMIT (int)5e8
 
 
-template <class M>
-void inc(M* mutex, int64_t limit, int64_t* sum, int64_t* thr_cnt) {
+template <class M,class T>
+void inc(M* mutex, int64_t limit, T& sum, int64_t& thr_cnt) {
     while(1) {
         std::lock_guard<M> g(*mutex);
-        if(*sum < limit) {
-            ++*sum;
-            ++*thr_cnt;
+        if(sum < limit) {
+            ++sum;
+            ++thr_cnt;
         } else {
             return;
         }
@@ -33,16 +33,16 @@ void inc(M* mutex, int64_t limit, int64_t* sum, int64_t* thr_cnt) {
 }
 
 
-template <class M>
+template <class M,class T>
 int64_t test_inc(int64_t limit, int thread_numb) {
     M mutex;
-    int64_t sum = 0;
-    std::vector <int64_t> counters(thread_numb,0);
+    T sum = 0;
+    std::vector<int64_t> counters(thread_numb,0);
     std::vector <std::thread> vt;
     std::chrono::time_point<std::chrono::system_clock> start, end;
     start = std::chrono::system_clock::now();
     for(int i = 0; i < thread_numb; ++i) {
-        vt.push_back(std::thread(inc<M>,&mutex,limit, &sum, &counters[i]));
+        vt.push_back(std::thread(inc<M,T>,&mutex,limit, std::ref(sum), std::ref(counters[i])));
     }
     
     for(auto& t : vt) {
@@ -53,42 +53,59 @@ int64_t test_inc(int64_t limit, int thread_numb) {
     int64_t elapsed_milliseconds = std::chrono::duration_cast<std::chrono::milliseconds>(end-start).count();
 
     
-//    assert(sum == limit);
-//    
-//    for(int i = 0;i < thread_numb;++i) {
-//        sum -= counters[i];
-//    }
-//    
-//    assert(sum == 0);
+    assert(sum == limit);
+    
+    for(int i = 0;i < thread_numb;++i) {
+        sum -= counters[i];
+    }
+    
+    assert(sum == 0);
     return elapsed_milliseconds;
 };
 
-template<class M>
+template<class M,class T>
 void test_increment_to_limit(int limit, int thread_numb) {
-
-    std::cout << "limit = " << limit << std::endl;
    
    //    for (int i = 1;i <= thread_numb; ++i) {
-    int i = 4;
-        int64_t elapsed_milliseconds = test_inc<M>(limit, i);
-                std::cout << "threads: " << i << " time: " << elapsed_milliseconds/1000 << " s" << std::endl;
+        int64_t elapsed_milliseconds = test_inc<M,T>(limit, thread_numb);
+                std::cout << "threads: " << thread_numb << " time: " << elapsed_milliseconds/1000 << " s" << std::endl;
 //    }
     std::cout << std::endl;
 }
 
 void case1() {
     //std::freopen("out.txt","w", stdout);
-    std::cout << "hardware_concurrency = " << std::thread::hardware_concurrency() << std::endl;
+    std::cout << "case without volatile:"<< std::endl<<std::endl;
+    
     std::cout << "test simple futex:" << std::endl;
-    test_increment_to_limit<futex>(LIMIT, MAX_THREADS);
+    test_increment_to_limit<futex,int64_t>(LIMIT, THREADS);
+    
     std::cout << "test futex with memory barriers for not volatile(strong):" << std::endl;
-    test_increment_to_limit<futex_with_memory_barriers_strong>(LIMIT, MAX_THREADS);
+    test_increment_to_limit<futex_with_memory_barriers_strong,int64_t>(LIMIT, THREADS);
+    
+    std::cout << std::endl;
+}
+
+void case2() {
+    std::cout << "case with volatile:"<< std::endl<<std::endl;
+    
+    std::cout << "test simple futex:" << std::endl;
+    test_increment_to_limit<futex,volatile int64_t>(LIMIT, THREADS);
+    
+    std::cout << "test futex with memory barriers for not volatile(strong):" << std::endl;
+    test_increment_to_limit<futex_with_memory_barriers_strong,volatile int64_t>(LIMIT, THREADS);
+    
     std::cout << "test futex with memory barriers for volatile(weak):" << std::endl;
-    test_increment_to_limit<futex_with_memory_barriers_weak>(LIMIT, MAX_THREADS);
+    test_increment_to_limit<futex_with_memory_barriers_weak, volatile int64_t>(LIMIT, THREADS);
+    
+    std::cout << std::endl;
 }
 
 int main(int argc, const char * argv[]) {
+    std::cout << "hardware_concurrency = " << std::thread::hardware_concurrency() <<std::endl;
+    std::cout << "limit = " << LIMIT << std::endl << std::endl;
     case1();
+    case2();
     return 0;
 }
 
